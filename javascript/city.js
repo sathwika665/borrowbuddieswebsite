344d3361689e4b4dffd7267b07febac8a42fc582
sathwika665 <136329649+sathwika665@users.noreply.github.com>
2024-10-27 06:57:53 +0530

    Delete javascript directory

--- javascript/city.js
+++
@@ -1,8 +0,0 @@
-// Select the image using its parent class or image ID
-const image = document.querySelector('.city-item img'); // or use document.getElementById('cityImage');
-
-// Add a click event listener to the image
-image.addEventListener('click', function() {
-    // Redirect to a different page on the website
-    window.location.href = '/htmlfiles/categories.html'; // Change this to your target page's path
-});
